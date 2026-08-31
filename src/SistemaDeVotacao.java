52f4753ff70748ae49c09bdb59032b1506892a0b
Maria Eduarda Albuquerque Nascimento <maria.11862855@aluno.mg.gov.br>
2026-08-31 13:59:21 -0300

    chore: criar estrutura inicial do programa

--- src/SistemaDeVotacao.java
+++ src/SistemaDeVotacao.java
@@ -1,14 +1,11 @@
 import java.util.Scanner;
 
 public class SistemaDeVotacao {
+
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Sistema de votação iniciado.");
+        scanner.close();
+    }
 }
-
-
-
-
-
-
-
-
-
-
